1fc862e8e993bfc45eef4153bd648fc950fd880e
Lorenzo Gentile <lorenzogentile404@gmail.com>
2018-09-20 17:51:18 +0200

    Introduced hashCost, cost and function to update decentralizationIndex

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ import numpy as np
 class Miner(Agent):
     def __init__(self, unique_id, hashRate, model):
         super().__init__(unique_id, model)
-        self.hashRate = hashRate
-        self.reward = 0
+        self.hashRate = hashRate # H/s
+        self.hashCost = 1 # e.g. Euro/H
+        self.reward = 0 # e.g. BTC
+        self.cost = 0 # e.g. Euro       
 
     def step(self):
-        print('miner:', self.unique_id, ',hashRate:', self.hashRate, ',reward:', self.reward)
-        # TODO: implement here miner policies
+        # Pay energy to compute hash
+        self.cost += self.hashRate * 10 * 60 * self.hashCost
+        
+        print('miner:', self.unique_id, ',hashRate:', self.hashRate, ',reward:', self.reward, ',cost:', self.cost)
+        # TODO: implement here miner policies taking into consideration (reward, cost, decentralizationIndex)
         
 class Network(Model):
     def __init__(self, numMiners, maxHashRate, initialReward):
@@ -21,6 +26,7 @@ class Network(Model):
         self.schedule = BaseScheduler(self)
         self.totalHashRate = 0
         self.reward = initialReward
+        self.decentralizationIndex = 0
         
         # Create miners
         for i in range(self.numMiners):
@@ -31,37 +37,47 @@ class Network(Model):
             
         # Declare data that have to be collected            
         self.datacollector = DataCollector(agent_reporters={"reward": "reward"})
-        
-    def step(self):
-        print('network')
-        # TODO: update here reward, difficulty        
-        
-        self.datacollector.collect(self)
+    
+    def powPuzzle(self):
         # Compute probability to solve pow for each miner        
         probabilityToSolvePowForEachMiner = list(map(lambda a: a.hashRate/self.totalHashRate, self.schedule.agents))
         # Select the winning miner        
         winningMinerIndex = np.random.choice(self.numMiners, 1, p = probabilityToSolvePowForEachMiner)
         # Reward the winning miner        
-        self.schedule.agents[winningMinerIndex].reward += self.reward     
+        self.schedule.agents[winningMinerIndex].reward += self.reward
+        
+    def updateDecentralizationIndex(self):
+        # TODO: self.decentralizationIndex = ...
+        print('update decentralizationIndex')
+        pass
+                
+    def step(self):
+        print('network before powPuzzle')               
+        # TODO: update here reward     
+        self.datacollector.collect(self)
+        self.powPuzzle()
         self.schedule.step()
+        print('network after powPuzzle, miner may have changed strategy')              
+        self.updateDecentralizationIndex()         
 
 # Run the simulation
 numMiners = 10
 maxHashRate = 100
-initialReward = 1     
+initialReward = 1
+steps = 10     
 network = Network(numMiners, maxHashRate, initialReward)
-for i in range(100):
+for i in range(steps):
     network.step()
 
 # Plot data regarding miners using datacollector
 minersRewards = network.datacollector.get_agent_vars_dataframe()
 # Plot reward of each miner for each step
-for i in range(10):
+for i in range(numMiners):
     oneMinerReward = minersRewards.xs(i, level="AgentID")
     #oneMinerReward.reward.plot()
     
 # Plot reward of each miner at the end of the simulation
-endMinersRewards = minersRewards.xs(99, level="Step")["reward"]
+endMinersRewards = minersRewards.xs(steps - 1, level="Step")["reward"]
 endMinersRewards.plot()
 
 
