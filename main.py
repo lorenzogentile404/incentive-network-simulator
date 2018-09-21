48bad1173745941d37c97726dfbe33ff51b9a364
Lorenzo Gentile <lorenzogentile404@gmail.com>
2018-09-21 18:37:05 +0200

    Added currencyValueWrtFiat to the model

--- main.py
+++ main.py
@@ -16,28 +16,31 @@ class Miner(Agent):
 
     def step(self):
         # Pay energy to compute hash
-        self.cost += self.hashRate * 10 * 60 * self.hashCost
-        
+        self.cost += self.hashRate * 10 * 60 * self.hashCost        
         print('miner:', self.unique_id, ',hashRate:', self.hashRate, ',reward:', self.reward, ',cost:', self.cost)
         # TODO: implement here miner policies taking into consideration (reward, cost, decentralizationIndex)
         
 class Network(Model):
-    def __init__(self, numMiners, maxHashRate, initialReward):
-        self.numMiners = numMiners
-        self.schedule = BaseScheduler(self)
-        self.totalHashRate = 0
-        self.reward = initialReward
+    def __init__(self, numMiners, maxHashRatePerMiner, initialReward, intialCurrencyValueWrtFiat):
+        self.numMiners = numMiners        
+        self.reward = initialReward # e.g. BTC
+        self.currencyValueWrtFiat = intialCurrencyValueWrtFiat # e.g. Euro        
+        self.totalHashRate = 0 # H/s
         self.decentralizationIndex = 0
+        self.schedule = BaseScheduler(self)
         
         # Create miners
         for i in range(self.numMiners):
-            hashRate = random.randrange(maxHashRate)
+            hashRate = random.randrange(maxHashRatePerMiner)
             # superMiner could have much more hashRate with respect to others
             #if i == 0:
-            #    hashRate *= 10   
+            #    hashRate *= 1   
             self.totalHashRate += hashRate
             a = Miner(i, hashRate, self)
             self.schedule.add(a)
+        
+        # Init decentralization index
+        self.computeDecentralizationIndex()         
             
         # Declare data that have to be collected            
         self.datacollector = DataCollector(agent_reporters={"reward": "reward"})
@@ -50,29 +53,40 @@ class Network(Model):
         # Reward the winning miner        
         self.schedule.agents[winningMinerIndex].reward += self.reward
         
-    def updateDecentralizationIndex(self):
+    def computeDecentralizationIndex(self):
         print('update decentralizationIndex')
         hashRates = list(map(lambda a: a.hashRate, network.schedule.agents))
         # as a decentralizationIndex 1 - gini indix is used
         # the higher it is, the more the hashRate is distributed equally among miners 
+        # TODO find a better index
         self.decentralizationIndex = 1 - pysal.inequality.gini.Gini(hashRates).g
-        print(self.decentralizationIndex)         
+        print(self.decentralizationIndex)
+ 
+    def computeCurrencyValueWrtFiat(self):    
+        print('update currencyValueWrtFiat')
+        # TODO find a reasonable relationship
+        self.currencyValueWrtFiat = self.currencyValueWrtFiat * (1 + (self.decentralizationIndex - 0.6)/4320)
+        print(self.currencyValueWrtFiat)        
                 
     def step(self):
         print('network before powPuzzle')               
-        # TODO: update here reward     
+        # TODO: update here the reward     
         self.datacollector.collect(self)
         self.powPuzzle()
         self.schedule.step()
-        print('network after powPuzzle, miner may have changed strategy')              
-        self.updateDecentralizationIndex()         
+        print('network after powPuzzle, miner may have changed strategy')
+        # Update decentralization index after streategies of miners may have changed               
+        self.computeDecentralizationIndex()       
+        # Update currency value with respect to fiat
+        self.computeCurrencyValueWrtFiat()
 
 # Run the simulation
 numMiners = 10
-maxHashRate = 100
+maxHashRatePerMiner = 100
 initialReward = 1
-steps = 100     
-network = Network(numMiners, maxHashRate, initialReward)
+intialCurrencyValueWrtFiat = 1
+steps = 4320 # in the case of Bitcoin each step is about 10 minutes, 4320 steps is about 1 month     
+network = Network(numMiners, maxHashRatePerMiner, initialReward, intialCurrencyValueWrtFiat)
 for i in range(steps):
     network.step()
 
