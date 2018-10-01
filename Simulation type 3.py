f663aa0039466e27a1ba7b9e3508e80dbd52c161
Lorenzo Gentile <lorenzogentile404@gmail.com>
2018-10-01 13:21:59 +0200

    Added simulation type 3

--- Simulation type 3.py
+++ Simulation type 3.py
@@ -4,7 +4,7 @@ mBreakdown = []
 kBreakdown = []
 
 
-for k in range(1,1000):
+for k in range(1,50,2):
     m = 1000
     while True:
         # Run the simulation
@@ -26,27 +26,27 @@ for k in range(1,1000):
         blockTime = 15 # s
         initialCurrencyValueWrtFiat = 200 # Euro        
         steps = 10 # In the case of Ethereum each step is about 15 seconds, 172800 steps is about 1 month     
-        np.random.seed(1) # If seed is not fixed there is noise in the plot, but the shape is the same
+        #np.random.seed(1) # TODO Investigate why plots with seed equal to 1 and 2 have different shape for low k
         # superMiningPool parameters are changed in order to simulate different scenarios
         # note that a lambda is used because in order to initialize an agent its model is required
         superMiningPool = lambda model: MiningPool(0, k, m, model)
         network = Network(superMiningPool, numMiningPools, averageNumMachinePerMiningPool, maxHashRatePerMachine, energyConsumptionPerMachine, initialReward, blockTime, initialCurrencyValueWrtFiat)
         for i in range(steps):
             network.step()          
-            if network.totalHashRate == network.schedule.agents[0].hashRate:
+            if network.totalHashRate == 0:
                 break
-        if network.totalHashRate == network.schedule.agents[0].hashRate:
+        if network.totalHashRate == 0:
             #print(str(k) + ' ' + str(m))
             kBreakdown.append(k)
             mBreakdown.append(m)
             break
-        m += 100
+        m += 1000
         
-plt.title('Only super mining pool is active for m and k', y=1.08)
+plt.title('No mining pool is active for m and k', y=1.08)
 plt.xlabel('k')
 plt.ylabel('m')
-plt.yscale('log')
+#plt.yscale('log')
 plt.plot(kBreakdown, mBreakdown)
-plt.savefig('plots/Only super mining pool is active for m and k', bbox_inches='tight')
+plt.savefig('plots/No mining pool is active for m and k', bbox_inches='tight')
 plt.clf()   
 
